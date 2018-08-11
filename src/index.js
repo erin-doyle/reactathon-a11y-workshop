ccf88615c99486b758d1d7ef9c25d7d25c0991ba
Erin Doyle <edoyle@cashstar.com>
2018-08-11 13:04:04 -0400

    Added eslint-plugin-jsx-a11y and react-a11y to lint and check for common accessibility findings

--- src/index.js
+++ src/index.js
@@ -6,6 +6,31 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
+// if we're in dev mode
+if (process.env.NODE_ENV === 'development') {
+    // Set up react-a11y to catch accessibility issues during development
+    const a11y = require('react-a11y').default;
+    const DEFAULT_LEVEL = 'warn'; // NOTE: change to 'error' if you want to raise errors instead of warnings
+    a11y(React, ReactDOM, {
+        rules: {
+            'button-role-space': DEFAULT_LEVEL,
+            'hidden-uses-tabindex': DEFAULT_LEVEL,
+            'img-uses-alt': DEFAULT_LEVEL,
+            'label-uses-for': DEFAULT_LEVEL,
+            'mouse-events-map-to-key-events': DEFAULT_LEVEL,
+            'no-access-key': DEFAULT_LEVEL,
+            'no-hash-ref': DEFAULT_LEVEL,
+            'no-unsupported-elements-use-aria': DEFAULT_LEVEL,
+            'onclick-uses-role': DEFAULT_LEVEL,
+            'onclick-uses-tabindex': DEFAULT_LEVEL,
+            'redundant-alt': DEFAULT_LEVEL,
+            'tabindex-uses-button': DEFAULT_LEVEL,
+            'use-onblur-not-onchange': DEFAULT_LEVEL,
+            'valid-aria-role': DEFAULT_LEVEL
+        }
+    });
+}
+
 ReactDOM.render((
     <BrowserRouter>
         <App/>
